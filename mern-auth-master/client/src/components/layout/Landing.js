c5a06427c059b34da289555e975b181f95f973a1
jventolero <jventolero.alterations-plus@gmail.com>
2022-08-19 08:07:09 +0800

    Removed Register

--- mern-auth-master/client/src/components/layout/Landing.js
+++ mern-auth-master/client/src/components/layout/Landing.js
@@ -17,19 +17,7 @@ class Landing extends Component {
               passport and JWTs
             </p>
             <br />
-            <div className="col s6">
-              <Link
-                to="/register"
-                style={{
-                  width: "140px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px"
-                }}
-                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-              >
-                Register
-              </Link>
-            </div>
+            
             <div className="col s6">
               <Link
                 to="/login"
